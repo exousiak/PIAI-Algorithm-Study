73408eb371ed4dc18db48cae17790b790c2743ab
noodlej <jmh010112@naver.com>
2024-02-05 20:44:09 +0900

    1주차 꿑

--- mh/week01/1.py
+++ mh/week01/1.py
@@ -1,1 +1,8 @@
-sdjkhfk lhg
+def solution(rsp):
+    dic = {'0': '5',
+          '2': '0',
+          '5': '2'}
+    answer=[]
+    for i in rsp:
+        answer.append(dic[i])
+    return ''.join(answer)
