ee4319aea61832bac2543d1d8e193b10ea1be328
KIM SEONMIN <143922629+SMplease@users.noreply.github.com>
2024-02-08 09:18:01 +0900

    Update 2. 250121. ［PCCE 기출문제］ 10번 ／ ［PCCE 기출문제］ 10번 ／ 데이터 분석.py

--- sm/week1/2번/2. 250121. ［PCCE 기출문제］ 10번 ／ ［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ sm/week1/2번/2. 250121. ［PCCE 기출문제］ 10번 ／ ［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -12,7 +12,11 @@ def solution(data, ext, val_ext, sort_by):
         elif ext == 'remain' and i[3] < val_ext:
             ans.append(i)
         
-### lambda에 대한 이해 : lambda x : x[0]   
+### lambda에 대한 이해 : lambda x : x[0]
+    # 아래서 쓰인 lambda함수는 ans 원소(?) 에 대한 정의
+    # x[0] = code , x[1] = date, x[2] = maximum, x[3] = remain
+### sort( key : key 기준 정렬   or    reverse=True : 역순정렬 )
+### 새로운 정렬된 리스트를 반환하는 함수는 'sorted 함수'이고, 리스트 자체를 정렬시켜버리는 것은 'sort 함수'
     if sort_by == 'code':
         ans.sort(key=lambda x: x[0])
     elif sort_by == 'date':
